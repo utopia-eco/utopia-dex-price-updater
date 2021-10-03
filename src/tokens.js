4f85f54cf4b7eb7e9b1d3bcedb99eecca0f051d1
derekzx <chinzhixian@gmail.com>
2021-10-03 14:06:52 +0800

    Fix uppercase tokens

--- src/tokens.js
+++ src/tokens.js
@@ -1,12 +1,12 @@
 module.exports = Object.freeze({
     TokenList: ["0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82", // Cake
-        "0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5", // Bake
+        "0xe02df9e3e622debdd69fb838bb799e3f168902c5", // Bake
         "0x8076c74c5e3f5852037f31ff0093eeb8c8add8d3", // Safemoon
         "0xaec945e04baf28b135fa7c640f624f8d90f1c3a6", // Coin98
         "0x111111111117dc0aa78b770fa6a738034120c302", // 1inch
         "0xfd7b3a77848f1c2d67e05e54d78d174a0c850335", // ONT
         "0x47bead2563dcbf3bf2c9407fea4dc236faba485a", // SXP
-        "0x1a1d7c7A92e8d7f0de10Ae532ECD9f63B7EAf67c", // UTP
+        "0x1a1d7c7A92e8d7f0de10Ae532ECD9f63B7EAf67c", // UTOPIA
         "0x2170ed0880ac9a755fd29b2688956bd959f933f8", // ETH
         "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c", // BTCB
         "0xe9e7cea3dedca5984780bafc599bd69add087d56", // BUSD
@@ -14,5 +14,5 @@ module.exports = Object.freeze({
         "0xcf6bb5389c92bdda8a3747ddb454cb7a64626c63", // XVS Venus
         "0x4bd17003473389a42daf6a0a729f6fdb328bbbd7", // VAI
         ]
-    // TokenList: ["0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"]
+    // TokenList: ["0x1a1d7c7A92e8d7f0de10Ae532ECD9f63B7EAf67c"]
 });
